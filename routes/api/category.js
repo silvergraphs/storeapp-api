be4b5b846235f40d270713c425413c18bcb4a713
Bruno Caruso <brunofcaruso@gmail.com>
2020-09-10 18:44:03 -0300

    CRUD de categorias
    Se creo el CRUD funcionando con Sequelize para las categorias

--- routes/api/category.js
+++ routes/api/category.js
@@ -2,105 +2,108 @@ var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
 
+const { Category } = require("../../db");
+
 // Configuring express to use body-parser as middle-ware.
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-// Create (POST)
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    res.send("No data received");
-  } else {
-    ADD_NEW_CATEGORY =
-      "INSERT INTO `Category` (`name`) VALUES ('" + name + "')";
-    connection.query(ADD_NEW_CATEGORY, (err, _category) => {
-      if (err) {
-        if (err.code === "ER_DUP_ENTRY") {
-          // Si el codigo de error es entrada duplicada
-          res.send("Category already exists"); // Avisa que la entrada ya existe
-        } else {
-          res.send(err);
-          console.log("[StoreApp] Error adding new category \n" + err);
-        }
-      } else {
-        res.send("Category added");
-        console.log(`[StoreApp] New category added to database (${name})`);
-      }
-    });
-  }
+// Read all categories (GET)
+router.get("/", async (req, res) => {
+  const categories = await Category.findAll();
+  res.json(categories);
 });
 
-// Read (GET)
-router.get("/", (_req, res) => {
-  SELECT_ALL_CATEGORIES = "SELECT * FROM `Category`";
-  connection.query(SELECT_ALL_CATEGORIES, (err, categories) => {
-    const data = {
-      data: categories,
-    };
-    if (err) {
-      return res.send(err);
+// Read specific category from ID (GET)
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const category = await Category.findOne({
+      where: { id: req.params.categoryId },
+    });
+    if (category) {
+      res.json(category);
     } else {
-      return res.json(data);
+      res.status(404).json({ success: false, message: "Category not found" });
     }
-  });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-// Update (PUT)
-router.put("/", (req, res) => {
-  let { categoryId, name } = req.body;
-  if (!categoryId || !name) {
-    res.send("No category ID received");
-  } else {
-    UPDATE_CATEGORY =
-      "UPDATE `Category` SET `name` = '" +
-      name +
-      "' WHERE `Category`.`idCategory` = " +
-      categoryId +
-      "";
-    connection.query(UPDATE_CATEGORY, (err, category) => {
-      if (err) {
-        if (err.code === "ER_DUP_ENTRY") {
-          // Si el codigo de error es entrada duplicada
-          res.send("Category name already exists"); // Avisa que la entrada ya existe
-        } else {
-          res.send(err);
-          console.log(
-            "[StoreApp] Error updating category ID " + categoryId + "\n" + err
-          );
-        }
+// Create category (POST)
+router.post("/", async (req, res) => {
+  if (req.body.name) {
+    try {
+      const [category, created] = await Category.findOrCreate({
+        where: { name: req.body.name },
+        defaults: {
+          name: req.body.name,
+        },
+      });
+      if (created) {
+        const { id, name } = category.dataValues;
+        res.json({
+          success: true,
+          msg: "Category created successfully",
+        });
+        console.log(
+          `[StoreApp] New category created (ID: ${id} - Name: ${name})`
+        );
       } else {
-        res.send("Category updated");
-        console.log(`[StoreApp] Category updated (ID: ${categoryId})`);
+        res.status(412).json({
+          success: false,
+          msg: "Category already exists",
+        });
       }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+  } else {
+    res.status(400).json({
+      success: false,
+      msg: "No enough data received",
     });
   }
 });
 
-// Delete (DELETE)
-router.delete("/", (req, res) => {
-  const { categoryId } = req.body;
-  if (!categoryId) {
-    res.send("No category ID received");
+// Update category (PUT)
+router.put("/:categoryId", async (req, res) => {
+  if (req.body.name) {
+    try {
+      await Category.update(req.body, {
+        where: { id: req.params.categoryId },
+      });
+      res.json({
+        success: true,
+        msg: "Category updated successfully",
+      });
+      console.log(
+        `[StoreApp] Category updated (ID: ${req.params.categoryId} - New name: ${req.body.name})`
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
   } else {
-    DELETE_CATEGORY =
-      "DELETE FROM `Category` WHERE `Category`.`idCategory` = " +
-      categoryId +
-      "";
-    connection.query(DELETE_CATEGORY, (err, _category) => {
-      if (err) {
-        res.send(err);
-        console.log(
-          "[StoreApp] Error deleting category ID " + categoryId + " \n" + err
-        );
-      } else {
-        res.send("Category deleted");
-        console.log(
-          `[StoreApp] category deleted from database (ID: ${categoryId})`
-        );
-      }
+    res.status(400).json({
+      success: false,
+      msg: "No enough data received",
     });
   }
 });
 
+// Delete category (DELETE)
+router.delete("/:categoryId", async (req, res) => {
+  try {
+    await Category.destroy({ where: { id: req.params.categoryId } });
+    res.json({
+      success: true,
+      msg: "Category deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+});
 module.exports = router;
