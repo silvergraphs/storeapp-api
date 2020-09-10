05bd2c91f6d7d61761c9a269dbb9689812eeba04
Bruno Caruso <brunofcaruso@gmail.com>
2020-09-10 14:39:31 -0300

    CRUDs y Readme
    Ya se pueden crear, leer, actualizar, y eliminar usuarios de la base de datos llamando a la API, tambien se corrigieron algunos errores en el README.md

--- README.md
+++ README.md
@@ -21,39 +21,39 @@ npm start
 
 #### Users
 
-`POST /api/user/` - Create new user
+`POST /api/user` - Create new user
 
-`GET /api/user/` - Get all users
+`GET /api/user` - Get all users
 
 `GET /api/user/id` - Get specific user from id
 
-`PUT /api/user/` - Updates user data
+`PUT /api/user/id` - Update specific user from id
 
-`DELETE /api/user/id` - Deletes specific user from id
+`DELETE /api/user/id` - Delete specific user from id
 
 #### Categories
 
-`POST /api/category/` - Create new category
+`POST /api/category` - Create new category
 
-`GET /api/category/` - Get all categories
+`GET /api/category` - Get all categories
 
 `GET /api/category/id` - Get specific category from id
 
-`PUT /api/category/` - Updates category data
+`PUT /api/category/id` - Update specific category from id
 
 `DELETE /api/category/id` - Deletes specific category from id
 
 #### Applications
 
-`POST /api/app/` - Create new application
+`POST /api/app` - Create new application
 
-`GET /api/app/` - Get all applications
+`GET /api/app` - Get all applications
 
 `GET /api/app/id` - Get specific application from id
 
-`PUT /api/app/` - Updates application data
+`PUT /api/app/id` - Update specific application from id
 
-`DELETE /api/app/id` - Deletes specific application from id
+`DELETE /api/app/id` - Delete specific application from id
 
 ## Contributing 👍
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const bodyParser = require("body-parser");
 
 const { User } = require("../../db");
+const e = require("express");
 
 // Configuring express to use body-parser as middle-ware.
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +13,19 @@ router.use(bodyParser.json());
 router.get("/", async (req, res) => {
   const users = await User.findAll();
   res.json(users);
+});
+
+// Read specific user from ID (GET)
+router.get("/:userId", async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { id: req.params.userId } });
+    user === null
+      ? res.json({ success: false, message: "User not found" })
+      : res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
 });
 
 // Create user (POST)
@@ -29,95 +43,70 @@ router.post("/", async (req, res) => {
       });
       if (created) {
         const { id, name, email, userType } = user.dataValues;
-        res.send("User created successfully");
+        res.json({
+          success: true,
+          msg: "User created successfully",
+        });
         console.log(
           `[StoreApp] New user created (ID: ${id} - Name: ${name} - Email: ${email} - User Type: ${userType})`
         );
       } else {
-        res.send("User email already exists");
+        res.json({
+          success: false,
+          msg: "User email already exists",
+        });
       }
     } catch (err) {
       console.error(err);
       res.send(err);
     }
   } else {
-    res.send("No enough data received");
-  }
-});
-
-/* // Read (GET)
-router.get("/", (_req, res) => {
-  SELECT_ALL_USERS = "SELECT * FROM `User`";
-  connection.query(SELECT_ALL_USERS, (err, users) => {
-    const data = {
-      data: users,
-    };
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.json(data);
-    }
-  });
-});
-
-// Update (PUT)
-router.put("/", (req, res) => {
-  let { userId, name, email } = req.body;
-  if (!userId) {
-    res.send("No user ID received");
-  } else {
-    UPDATE_USER = "UPDATE `User` SET ";
-    if (name) {
-      name = "`name` = '" + name + "', ";
-      UPDATE_USER += name;
-    }
-    if (email) {
-      email = "`email` = '" + email + "' ";
-      UPDATE_USER += email;
-    }
-    if (!name && !email) {
-      res.send("Email or name required");
-    } else {
-      UPDATE_USER += "WHERE `User`.`idUser` = " + userId + " ";
-    }
-    connection.query(UPDATE_USER, (err, user) => {
-      if (err) {
-        if (err.code === "ER_DUP_ENTRY") {
-          // Si el codigo de error es entrada duplicada
-          res.send("Email already exists"); // Avisa que el usuario ya existe
-        } else {
-          res.send(err);
-          console.log(
-            "[StoreApp] Error updating user ID " + userId + "\n" + err
-          );
-        }
-      } else {
-        res.send("User updated");
-        console.log(`[StoreApp] User updated (ID: ${userId} - ${email})`);
-      }
+    res.json({
+      success: false,
+      msg: "No enough data received",
     });
   }
 });
 
-// Delete (DELETE)
-router.delete("/", (req, res) => {
-  const { userId } = req.body;
-  if (!userId) {
-    res.send("No user ID received");
+// Update user (PUT)
+router.put("/:userId", async (req, res) => {
+  const { name, email, userType } = req.body;
+  if (name && email && userType) {
+    try {
+      await User.update(req.body, {
+        where: { id: req.params.userId },
+      });
+      res.json({
+        success: true,
+        msg: "User updated successfully",
+      });
+      console.log(
+        `[StoreApp] User updated (ID: ${req.params.userId} - Name: ${name} - Email: ${email} - User Type: ${userType})`
+      );
+    } catch (err) {
+      console.error(err);
+      res.send(err);
+    }
   } else {
-    DELETE_USER = "DELETE FROM `User` WHERE `User`.`idUser` = " + userId + "";
-    connection.query(DELETE_USER, (err, _user) => {
-      if (err) {
-        res.send(err);
-        console.log(
-          "[StoreApp] Error deleting user ID " + userId + " \n" + err
-        );
-      } else {
-        res.send("User deleted");
-        console.log(`[StoreApp] User deleted from database (ID: ${userId})`);
-      }
+    res.json({
+      success: false,
+      msg: "No enough data received",
     });
   }
-}); */
+});
+
+// Delete user (DELETE)
+router.delete("/:userId", async (req, res) => {
+  try {
+    await User.destroy({ where: { id: req.params.userId } });
+    res.json({
+      success: true,
+      msg: "User deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+});
 
 module.exports = router;
